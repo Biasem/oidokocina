f4d8d32b9d8d579742e5f477065f545e70be2bbc
PC38\daw20 <rnietourena@safareyes.es>
2022-04-01 16:04:44 +0200

    panel fondo joder

--- src/main/java/ConstruirRestaurante.java
+++ src/main/java/ConstruirRestaurante.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.io.File;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 
 public class ConstruirRestaurante {
     private static final Image urlimg = new ImageIcon(geturlimg()).getImage();
     private JFrame ventana;
     private JPanel panel,panelprincipal;
-    private JTextArea areadetexto;
-    private JScrollPane barralateral;
-    private JTextField cajatexto;
-    private JLabel etiqueta;
-    private JButton boton;
+    private JButton camarero,admin,cliente,cocinero;
 
     ConstruirRestaurante(){
     ConstruirVentana();
@@ -23,12 +22,12 @@ public class ConstruirRestaurante {
         ventana = new JFrame("OidoKocina");
         ventana.setSize(800,600);
         ventana.setLocationRelativeTo(null);
-
+        ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         PanelFondo();
-        PanelConstruir();
         ventana.setContentPane(panel);
         ventana.setVisible(true);
+
 
     }
 
@@ -48,22 +47,27 @@ public class ConstruirRestaurante {
                 super.paint(g);
             }
         };
+
         panel.setOpaque(false);
 
-        JLabel prueba = new JLabel("ave aveeee");
-        panel.add(prueba);
+
+        cocinero = new JButton("Cocinero");
+        camarero = new JButton("Camarero");
+        admin = new JButton("Administrador");
+        cliente = new JButton("Cliente");
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(new EmptyBorder(100,100,100,100));
+
+
+        panel.add(cocinero,BorderLayout.NORTH);
+        panel.add(camarero,BorderLayout.WEST);
+        panel.add(cliente, BorderLayout.EAST);
+        panel.add(admin, BorderLayout.SOUTH);
 
 
     }
 
-    private void PanelConstruir()   //panel principal de bienvenida
-    {
-        panelprincipal = new JPanel();
-        panelprincipal.setLayout(null);
-        JLabel fondo = new JLabel("hola holita");
-        panelprincipal.setOpaque(false);
 
-    }
     private static String geturlimg(){
         String ruta = new File("").getAbsolutePath();
         return ruta + "\\src\\main\\imagenes\\imagen_principal.jpg";
