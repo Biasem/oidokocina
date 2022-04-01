90a17b456ed22b0fbe126ef15a8274e39c28e039
jmcamacho7 <camachosanchezjm@gmail.com>
2022-04-01 17:45:37 +0200

    Primeros cambios

--- src/main/java/ConstruirRestaurante.java
+++ src/main/java/ConstruirRestaurante.java
@@ -4,6 +4,21 @@ import java.awt.*;
 import java.io.File;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.border.AbstractBorder;
+import java.awt.*;
+
 
 
 public class ConstruirRestaurante {
@@ -12,15 +27,18 @@ public class ConstruirRestaurante {
     private JPanel panel,panelprincipal;
     private JButton camarero,admin,cliente,cocinero;
 
+
     ConstruirRestaurante(){
     ConstruirVentana();
 
     }
 
+
     private void ConstruirVentana()
     {                                               //se construye la ventana y panel principal
         ventana = new JFrame("OidoKocina");
         ventana.setSize(800,600);
+        ventana.setResizable(false);
         ventana.setLocationRelativeTo(null);
         ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -29,6 +47,15 @@ public class ConstruirRestaurante {
         ventana.setVisible(true);
 
 
+
+
+    }
+
+    public class seticonimg extends javax.swing.JFrame{
+
+        public seticonimg() {
+
+        }
     }
 
     private void RestaurarPanel()   //metodo para inicializar paneles
@@ -50,7 +77,6 @@ public class ConstruirRestaurante {
 
         panel.setOpaque(false);
 
-
         cocinero = new JButton("Cocinero");
         camarero = new JButton("Camarero");
         admin = new JButton("Administrador");
@@ -64,13 +90,31 @@ public class ConstruirRestaurante {
         panel.add(cliente, BorderLayout.EAST);
         panel.add(admin, BorderLayout.SOUTH);
 
+        cocinero.setBorderPainted(false);
+        cocinero.setFocusPainted(false);
+        cocinero.setContentAreaFilled(true);
+
+        camarero.setBorderPainted(false);
+        camarero.setFocusPainted(false);
+        camarero.setContentAreaFilled(true);
+
+        admin.setBorderPainted(false);
+        admin.setFocusPainted(false);
+        admin.setContentAreaFilled(true);
+
+        cliente.setBorderPainted(false);
+        cliente.setFocusPainted(false);
+        cliente.setContentAreaFilled(true);
+
 
     }
 
 
     private static String geturlimg(){
         String ruta = new File("").getAbsolutePath();
-        return ruta  + "\\src\\main\\imagenes\\imagen_principal.jpg";
+        return ruta  + "\\src\\main\\imagenes\\menuprincipal.jpg";
     }
+
+
 
 }
