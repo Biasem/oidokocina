4eb7d0d362528d600188e3ca1124293db5e8df44
jmcamacho7 <camachosanchezjm@gmail.com>
2022-04-01 20:31:50 +0200

    iconos puestos

--- src/main/java/ConstruirRestaurante.java
+++ src/main/java/ConstruirRestaurante.java
@@ -37,7 +37,7 @@ public class ConstruirRestaurante {
     private void ConstruirVentana()
     {                                               //se construye la ventana y panel principal
         ventana = new JFrame("OidoKocina");
-        ventana.setSize(800,600);
+        ventana.setSize(1920,1080);
         ventana.setResizable(false);
         ventana.setLocationRelativeTo(null);
         ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -77,10 +77,46 @@ public class ConstruirRestaurante {
 
         panel.setOpaque(false);
 
+
+        /// Botón cocinero
         cocinero = new JButton("Cocinero");
+        String ruta = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\cocinero.png" ;
+        ImageIcon imagen = new ImageIcon(ruta);
+        Image imagenLimitadaTamanyo = imagen.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
+        imagen.setImage(imagenLimitadaTamanyo);
+        cocinero.setIcon(imagen);
+        cocinero.setFocusPainted(true);
+        cocinero.setBackground(Color.DARK_GRAY);
+
+        /// Botón camarero
         camarero = new JButton("Camarero");
+        ruta = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\camarero.png" ;
+        imagen = new ImageIcon(ruta);
+        imagenLimitadaTamanyo = imagen.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
+        imagen.setImage(imagenLimitadaTamanyo);
+        camarero.setIcon(imagen);
+        camarero.setFocusPainted(true);
+
+        /// Botón admin
+
         admin = new JButton("Administrador");
+        ruta = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\apoyo.png" ;
+        imagen = new ImageIcon(ruta);
+        imagenLimitadaTamanyo = imagen.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
+        imagen.setImage(imagenLimitadaTamanyo);
+        admin.setIcon(imagen);
+        admin.setFocusPainted(true);
+
+        /// Botón cliente
+
         cliente = new JButton("Cliente");
+        ruta = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\cliente.png" ;
+        imagen = new ImageIcon(ruta);
+        imagenLimitadaTamanyo = imagen.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
+        imagen.setImage(imagenLimitadaTamanyo);
+        cliente.setIcon(imagen);
+        cliente.setFocusPainted(true);
+
         panel.setLayout(new BorderLayout());
         panel.setBorder(new EmptyBorder(100,100,100,100));
 
