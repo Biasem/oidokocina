5d9ef39e76e3377681fab277591d9d30de11129a
PC38\daw20 <rnietourena@safareyes.es>
2022-04-01 17:35:14 +0200

    cambio imagen fondo

--- src/main/java/ConstruirRestaurante.java
+++ src/main/java/ConstruirRestaurante.java
@@ -1,16 +1,20 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.awt.Graphics;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 
-public class ConstruirRestaurante {
-    private static final Image urlimg = new ImageIcon(geturlimg()).getImage();
+public class ConstruirRestaurante extends JFrame{
+    private static Image urlImagen = new ImageIcon(getUrlImagenInicio()).getImage();
     private JFrame ventana;
     private JPanel panel,panelprincipal;
     private JButton camarero,admin,cliente,cocinero;
+
 
     ConstruirRestaurante(){
     ConstruirVentana();
@@ -24,26 +28,26 @@ public class ConstruirRestaurante {
         ventana.setLocationRelativeTo(null);
         ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        PanelFondo();
+        panelInicio();
         ventana.setContentPane(panel);
         ventana.setVisible(true);
 
 
     }
 
-    private void RestaurarPanel()   //metodo para inicializar paneles
+    private void restaurarPanel()   //metodo para inicializar paneles
     {
         panel.removeAll();
         panel.repaint();
         panel.revalidate();
     }
 
-    private void PanelFondo()   //panel principal de bienvenida
+    private void panelInicio()   //panel principal de bienvenida
     {
         panel = new JPanel(){
             @Override
             public void paint(Graphics g){
-                g.drawImage(urlimg, 0, 0, getWidth(), getHeight(), this);
+                g.drawImage(urlImagen, 0, 0, getWidth(), getHeight(), this);
                 super.paint(g);
             }
         };
@@ -62,15 +66,45 @@ public class ConstruirRestaurante {
         panel.add(cocinero,BorderLayout.NORTH);
         panel.add(camarero,BorderLayout.WEST);
         panel.add(cliente, BorderLayout.EAST);
+        ActionListener oyenteCliente = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelcliente();
+            }
+        };
+        cliente.addActionListener(oyenteCliente);
+
         panel.add(admin, BorderLayout.SOUTH);
 
 
     }
 
 
-    private static String geturlimg(){
+    private static String getUrlImagenInicio(){
         String ruta = new File("").getAbsolutePath();
         return ruta + "\\src\\main\\imagenes\\imagen_principal.jpg";
     }
+    private static String getUrlImagenCliente(){
+        String ruta = new File("").getAbsolutePath();
+        return ruta + "\\src\\main\\imagenes\\carta.png";
+    }
+
+    private void panelcliente (){
+        urlImagen = new ImageIcon(getUrlImagenCliente()).getImage();
+        restaurarPanel();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 
 }
