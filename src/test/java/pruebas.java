48018cace696943d1dc492e09b8a698d4ee3c41b
PC38\daw20 <rnietourena@safareyes.es>
2022-04-05 18:53:17 +0200

    Conexion con bbdd

--- src/test/java/pruebas.java
+++ src/test/java/pruebas.java
@@ -1,13 +1,19 @@
+import Modelos.Producto;
+import UtilidadesBBDD.ProductoBD;
+import UtilidadesBBDD.UtilidadesBD;
+
 import java.awt.Dimension;
+import java.sql.Connection;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class pruebas {
+public class pruebas extends UtilidadesBD{
 
     public static void main(String... args) {
+        System.out.println(ProductoBD.obtenerProducto());
 
     }
 }
