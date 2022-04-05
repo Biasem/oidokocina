fcc2e29364108b0eb11c01004f5ce48f795528e2
PC38\daw20 <rnietourena@safareyes.es>
2022-04-05 16:38:14 +0200

    creando utilidades procuctos

--- src/main/java/Modelos/Producto.java
+++ src/main/java/Modelos/Producto.java
@@ -1,6 +1,7 @@
 package Modelos;
 
 import UtilidadesBBDD.UtilidadesBD;
+import java.util.Objects;
 
 public class Producto {
 
@@ -9,8 +10,65 @@ public class Producto {
     private Double  precio;
     private TipoProducto tipoProducto;
 
+    public Producto(int id, String descripcion, Double precio, TipoProducto tipoProducto) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.tipoProducto = tipoProducto;
+    }
+
+    public Producto() {
+    }
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public TipoProducto getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(TipoProducto tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return id == producto.id && Objects.equals(descripcion, producto.descripcion) && Objects.equals(precio, producto.precio) && tipoProducto == producto.tipoProducto;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descripcion, precio, tipoProducto);
+    }
+
+    @Override
+    public String toString() {
+        return descripcion + "\t" + "\t"  + precio;
+    }
 }
