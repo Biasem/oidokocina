6c01b4519093c01d295a9426ea28df4c536b1b50
Ruben <rnieto@safareyes.es>
2022-05-10 12:44:44 +0200

    objetos factura y lineacomanda

--- src/main/java/Modelos/Factura.java
+++ src/main/java/Modelos/Factura.java
@@ -1,4 +1,75 @@
 package Modelos;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Factura {
+    private int id;
+    private int id_mesa;
+    private LocalDate fecha;
+    private Double total;
+
+    public Factura(int id, int id_mesa, LocalDate fecha, Double total) {
+        this.id = id;
+        this.id_mesa = id_mesa;
+        this.fecha = fecha;
+        this.total = total;
+    }
+
+    public Factura(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_mesa() {
+        return id_mesa;
+    }
+
+    public void setId_mesa(int id_mesa) {
+        this.id_mesa = id_mesa;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Factura factura = (Factura) o;
+        return id == factura.id && id_mesa == factura.id_mesa && Objects.equals(fecha, factura.fecha) && Objects.equals(total, factura.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, id_mesa, fecha, total);
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "id=" + id +
+                ", id_mesa=" + id_mesa +
+                ", fecha=" + fecha +
+                ", total=" + total +
+                '}';
+    }
 }
--- src/main/java/Modelos/LineaComanda.java
+++ src/main/java/Modelos/LineaComanda.java
@@ -1,4 +1,96 @@
 package Modelos;
 
+import java.util.Objects;
+
 public class LineaComanda {
+    private int id;
+    private int idEmpleado;
+    private int idFactura;
+    private int idProducto;
+    private int cantidadProducto;
+    private int cantidadCocinada;
+
+    public LineaComanda(int id, int idEmpleado, int idFactura, int idProducto, int cantidadProducto, int cantidadCocinada) {
+        this.id = id;
+        this.idEmpleado = idEmpleado;
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.cantidadProducto = cantidadProducto;
+        this.cantidadCocinada = cantidadCocinada;
+    }
+
+    public LineaComanda(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(int idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+    public void setCantidadProducto(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
+    }
+
+    public int getCantidadCocinada() {
+        return cantidadCocinada;
+    }
+
+    public void setCantidadCocinada(int cantidadCocinada) {
+        this.cantidadCocinada = cantidadCocinada;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineaComanda that = (LineaComanda) o;
+        return id == that.id && idEmpleado == that.idEmpleado && idFactura == that.idFactura && idProducto == that.idProducto && cantidadProducto == that.cantidadProducto && cantidadCocinada == that.cantidadCocinada;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, idEmpleado, idFactura, idProducto, cantidadProducto, cantidadCocinada);
+    }
+
+    @Override
+    public String toString() {
+        return "LineaComanda{" +
+                "id=" + id +
+                ", idEmpleado=" + idEmpleado +
+                ", idFactura=" + idFactura +
+                ", idProducto=" + idProducto +
+                ", cantidadProducto=" + cantidadProducto +
+                ", cantidadCocinada=" + cantidadCocinada +
+                '}';
+    }
 }
