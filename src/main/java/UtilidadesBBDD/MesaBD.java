f129e6d3ccf99097e55e153376eaf454df40ecd4
PC38\daw20 <rnietourena@safareyes.es>
2022-05-13 19:50:44 +0200

    arreglos bbdd

--- src/main/java/UtilidadesBBDD/MesaBD.java
+++ src/main/java/UtilidadesBBDD/MesaBD.java
@@ -1,5 +1,67 @@
 package UtilidadesBBDD;
 
+import Modelos.Mesa;
+import Modelos.Producto;
+import Modelos.TipoProducto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MesaBD extends UtilidadesBD{
+
+    public static List<Mesa> obtenerTodasMesas() {
+        List<Mesa> milista = new ArrayList<>();
+        Connection con = conectarConBD();
+        Mesa mesa;
+
+        try {
+            PreparedStatement query = con.prepareStatement("SELECT * FROM mesa  ");
+            ResultSet rs = query.executeQuery();
+
+            //Recorremos los datos
+            while (rs.next()) {
+                mesa = new Mesa(rs.getInt("id"), rs.getInt("num_mesa"),
+                        rs.getInt("num_comensales"), rs.getBoolean("ocupada"));
+                milista.add(mesa);
+            }
+
+        } catch (SQLException sqle) {
+            System.out.println("Error en la ejecución:"
+                    + sqle.getErrorCode() + " " + sqle.getMessage());
+
+        } finally {
+            cerrarConexion(con);
+        }
+        return milista;
+    }
+
+    public static void ocuparMesa(int numMesa){
+
+        Connection con = conectarConBD();
+
+        try {
+            PreparedStatement insert = con.prepareStatement("update mesa set ocupada = 1 " +
+                    "where num_mesa =? ");
+
+            insert.setInt(1, numMesa);
+
+            //Ejecución del insert
+            insert.executeUpdate();
+
+
+        } catch (SQLException sqle) {
+            System.out.println("Error en la ejecución:"
+                    + sqle.getErrorCode() + " " + sqle.getMessage());
+
+        } finally {
+            cerrarConexion(con);
+        }
+    }
+
+
 
 }
