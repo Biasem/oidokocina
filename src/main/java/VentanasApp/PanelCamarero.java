f27ab79ef170d990240df1f21991745d4f37705d
jmcamacho7 <camachosanchezjm@gmail.com>
2022-05-19 20:13:48 +0200

    Visual pedidos acabada

--- src/main/java/VentanasApp/PanelCamarero.java
+++ src/main/java/VentanasApp/PanelCamarero.java
@@ -33,59 +33,52 @@ public class PanelCamarero extends JPanel {
         PanelPrincipal.RestaurarPanel(panel);
         panel.setLayout(null);
 
+        ArrayList<JButton> listabotones = new ArrayList<>();
+
         //boton de aforo
         JButton aforo = new JButton("Aforo");
         aforo.setBounds(230, 500, 250, 100);
         ActionListener oyenteAforo = e -> panelAforo(panel);
         aforo.addActionListener(oyenteAforo);
-        aforo.setBorderPainted(true);
-        aforo.setFocusPainted(true);
-        aforo.setContentAreaFilled(true);
-        aforo.setBorder(BorderFactory.createMatteBorder(
-                1, 1, 1, 1, Color.darkGray));
-        aforo.setBackground(Color.WHITE);
+
+        listabotones.add(aforo);
+
         String ruta6 = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\crowd.png" ;
         ImageIcon imagen6 = new ImageIcon(ruta6);
         Image imagenLimitadaTamanyo = imagen6.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
         imagen6.setImage(imagenLimitadaTamanyo);
         aforo.setIcon(imagen6);
-        panel.add(aforo);
 
         //boton de pedidos
         JButton pedidos = new JButton("Pedidos");
         pedidos.setBounds(730, 500, 250, 100);
         ActionListener oyentepedidos = e -> panelPedidos(panel);
         pedidos.addActionListener(oyentepedidos);
-        pedidos.setBorderPainted(true);
-        pedidos.setFocusPainted(true);
-        pedidos.setContentAreaFilled(true);
-        pedidos.setBorder(BorderFactory.createMatteBorder(
-                1, 1, 1, 1, Color.darkGray));
-        pedidos.setBackground(Color.WHITE);
+
+        listabotones.add(pedidos);
+
         String ruta3 = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\products.png" ;
         ImageIcon imagen3 = new ImageIcon(ruta3);
         Image imagenLimitadaTamanyo3 = imagen3.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
         imagen3.setImage(imagenLimitadaTamanyo3);
         pedidos.setIcon(imagen3);
-        panel.add(pedidos);
 
         //boton de cuentas
         JButton cuentas = new JButton("Cuentas");
         cuentas.setBounds(480, 500, 250, 100);
         ActionListener oyentecuentas = e -> panelCuentas(panel);
         cuentas.addActionListener(oyentecuentas);
-        cuentas.setBorderPainted(true);
-        cuentas.setFocusPainted(true);
-        cuentas.setContentAreaFilled(true);
-        cuentas.setBorder(BorderFactory.createMatteBorder(
-                1, 1, 1, 1, Color.darkGray));
-        cuentas.setBackground(Color.WHITE);
+
+        listabotones.add(cuentas);
+
         String ruta4 = new File("").getAbsolutePath() + "\\src\\main\\imagenes\\bill.png" ;
         ImageIcon imagen4 = new ImageIcon(ruta4);
         Image imagenLimitadaTamanyo4 = imagen4.getImage().getScaledInstance(60, 60,  java.awt.Image.SCALE_SMOOTH);
         imagen4.setImage(imagenLimitadaTamanyo4);
         cuentas.setIcon(imagen4);
-        panel.add(cuentas);
+
+        metodos.plantillaboton(listabotones, panel);
+
         //boton atras
         PanelPrincipal.botonAtras();
     }
@@ -233,46 +226,46 @@ public class PanelCamarero extends JPanel {
         JLabel labelMesa = new JLabel("Mesa:");
         labelMesa.setFont( new Font("TimesRoman",Font.BOLD,20));
         labelMesa.setForeground(Color.white);
-        labelMesa.setBounds(50,100,70,20);
+        labelMesa.setBounds(50,150,70,20);
         panel.add(labelMesa);
         //Campo Mesa
         JTextField campoMesa = new JTextField();
         campoMesa.addKeyListener(new FiltroNumeros());
-        campoMesa.setBounds(110,100,50,20);
+        campoMesa.setBounds(110,150,50,20);
         panel.add(campoMesa);
 
         //Label Camarero
         JLabel labelCamarero = new JLabel("Camarero:");
         labelCamarero.setFont( new Font("TimesRoman",Font.BOLD,20));
         labelCamarero.setForeground(Color.white);
-        labelCamarero.setBounds(250,100,100,20);
+        labelCamarero.setBounds(50,350,100,20);
         panel.add(labelCamarero);
         //Campo Camarero
         JTextField campoCamarero = new JTextField();
         campoCamarero.addKeyListener(new FiltroNumeros());
-        campoCamarero.setBounds(350,100,50,20);
+        campoCamarero.setBounds(170,350,50,20);
         panel.add(campoCamarero);
 
         //Label Producto
         JLabel labelProducto = new JLabel("Producto:");
         labelProducto.setFont( new Font("TimesRoman",Font.BOLD,20));
         labelProducto.setForeground(Color.white);
-        labelProducto.setBounds(50,200,100,20);
+        labelProducto.setBounds(50,250,100,20);
         panel.add(labelProducto);
         //Combo Producto
         JComboBox comboProducto = new JComboBox();
-        comboProducto.setBounds(150,200,250,20);
+        comboProducto.setBounds(150,250,250,20);
         panel.add(comboProducto);
 
         //Label Tipo Producto
         JLabel labelTipoProducto = new JLabel("Tipo:");
         labelTipoProducto.setFont( new Font("TimesRoman",Font.BOLD,20));
         labelTipoProducto.setForeground(Color.white);
-        labelTipoProducto.setBounds(50,150,100,20);
+        labelTipoProducto.setBounds(50,200,100,20);
         panel.add(labelTipoProducto);
         //Combo Tipo Producto
         JComboBox comboTipoProducto = new JComboBox();
-        comboTipoProducto.setBounds(100,150,140,20);
+        comboTipoProducto.setBounds(100,200,140,20);
         comboTipoProducto.addItem(TipoProducto.TAPA);
         comboTipoProducto.addItem(TipoProducto.MEDIA);
         comboTipoProducto.addItem(TipoProducto.RACION);
@@ -293,18 +286,19 @@ public class PanelCamarero extends JPanel {
         JLabel labelCantidad = new JLabel("Cantidad:");
         labelCantidad.setFont( new Font("TimesRoman",Font.BOLD,20));
         labelCantidad.setForeground(Color.white);
-        labelCantidad.setBounds(50,250,100,20);
+        labelCantidad.setBounds(50,300,100,20);
         panel.add(labelCantidad);
         //Campo Cantidad
         JTextField campoCantidad = new JTextField();
         campoCantidad.addKeyListener(new FiltroNumeros());
-        campoCantidad.setBounds(150,250,50,20);
+        campoCantidad.setBounds(150,300,50,20);
         panel.add(campoCantidad);
 
-
+        ArrayList<JButton> botones = new ArrayList<>();
         //Boton añadir linea comanda
         JButton aniadirProducto = new JButton("Añadir");
-        aniadirProducto.setBounds(300,350,100,50);
+        botones.add(aniadirProducto);
+        aniadirProducto.setBounds(650,600,100,50);
         ActionListener accionAniadirProducto = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -345,10 +339,11 @@ public class PanelCamarero extends JPanel {
             }
         };
         aniadirProducto.addActionListener(accionAniadirProducto);
-        panel.add(aniadirProducto);
+
 
         //Boton tramitar comanda
         JButton enviarComanda = new JButton("Enviar Comanda");
+        botones.add(enviarComanda);
         ActionListener accionEnviarComanda = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -358,13 +353,14 @@ public class PanelCamarero extends JPanel {
             }
         };
         enviarComanda.addActionListener(accionEnviarComanda);
-        enviarComanda.setBounds(600,500,300,50);
-        panel.add(enviarComanda);
+        enviarComanda.setBounds(750,600,200,50);
+
 
         JPanel panel2 = new JPanel();
         panel2.setLayout(null);
         //productos en botones para poner bonico
 
+        metodos.plantillaboton(botones, panel);
         panel2.setOpaque(false);
         JScrollPane scrollPane = new JScrollPane(panel2);
         int bajadaLabel = 0;
@@ -382,7 +378,7 @@ public class PanelCamarero extends JPanel {
         }
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(400, 0, 780, 500);// aqui se puede ajustar los parametros del scrool
+        scrollPane.setBounds(500, 50, 600, 500);// aqui se puede ajustar los parametros del scrool
         scrollPane.setOpaque(false);
         //scrollPane.getViewport().setOpaque(false);
         panel.add(scrollPane);
